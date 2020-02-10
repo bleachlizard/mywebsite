21554ef55605af94b310bfea47fcc85cf671bd71
Paul Pemberton <caps.pemberton@gmail.com>
2020-02-09 19:52:20 -0800

    Copied template files to wwwroot, and created LESS

--- MyWebsite/MyWebsite.Site/wwwroot/js/site.es5.js
+++ MyWebsite/MyWebsite.Site/wwwroot/js/site.es5.js
@@ -1,4 +1,6 @@
-﻿(function ($) {
+﻿'use strict';
+
+(function ($) {
     "use strict";
 
     // Preloader
@@ -144,16 +146,14 @@
     var introCarousel = $(".carousel");
     var introCarouselIndicators = $(".carousel-indicators");
     introCarousel.find(".carousel-inner").children(".carousel-item").each(function (index) {
-        (index === 0) ?
-            introCarouselIndicators.append("<li data-target='#introCarousel' data-slide-to='" + index + "' class='active'></li>") :
-            introCarouselIndicators.append("<li data-target='#introCarousel' data-slide-to='" + index + "'></li>");
+        index === 0 ? introCarouselIndicators.append("<li data-target='#introCarousel' data-slide-to='" + index + "' class='active'></li>") : introCarouselIndicators.append("<li data-target='#introCarousel' data-slide-to='" + index + "'></li>");
 
         $(this).css("background-image", "url('" + $(this).children('.carousel-background').children('img').attr('src') + "')");
         $(this).children('.carousel-background').remove();
     });
 
     $(".carousel").swipe({
-        swipe: function (event, direction, distance, duration, fingerCount, fingerData) {
+        swipe: function swipe(event, direction, distance, duration, fingerCount, fingerData) {
             if (direction == 'left') $(this).carousel('next');
             if (direction == 'right') $(this).carousel('prev');
         },
@@ -203,6 +203,5 @@
         loop: true,
         items: 1
     });
-
 })(jQuery);
 
